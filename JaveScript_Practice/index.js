f78055f764f73458cdea621638e8caf0e2d11950
Noibi Wahab Abiodun <noibiwahababiodun@gmail.com>
2025-05-16 01:27:38 +0100

    new

---
+++ JaveScript_Practice/index.js
@@ -0,0 +1,13 @@
+"use strict"; 
+
+let fullName ="John Doe"; 
+fullName = "Jane Doe";
+let age = 30;
+age = 30;
+
+isMale = false
+let uniqueid = Symbol("id");
+let cohort = null;
+let startDate = undefined;
+
+console.log(fullName,age, startDate);
